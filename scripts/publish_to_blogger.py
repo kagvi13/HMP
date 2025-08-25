b7d2016348c1b6eb7ead871b04080e441db173d7
kagvi13 <kagvi13@gmail.com>
2025-08-25 19:22:18 +0300

    Update publish_to_blogger.py

--- scripts/publish_to_blogger.py
+++ scripts/publish_to_blogger.py
@@ -1,17 +1,19 @@
 import os
 import pickle
+import base64
 from googleapiclient.discovery import build
 import markdown2
 
-# Загружаем OAuth токен
-with open('token.pkl', 'rb') as f:
-    creds = pickle.load(f)
+# Загружаем OAuth токен из GitHub Secrets (base64)
+token_b64 = os.environ['TOKEN_PKL']
+token_bytes = base64.b64decode(token_b64)
+creds = pickle.loads(token_bytes)
 
 service = build('blogger', 'v3', credentials=creds)
-BLOG_ID = os.environ['BLOGGER_BLOG_ID']  # Укажем через GitHub Secrets
+BLOG_ID = os.environ['BLOGGER_BLOG_ID']
 
 # Папка с Markdown-файлами для публикации
-POSTS_DIR = 'docs'
+POSTS_DIR = os.path.join(os.path.dirname(__file__), '..', 'docs')
 
 for filename in os.listdir(POSTS_DIR):
     if filename.endswith('.md'):
